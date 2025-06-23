be47ec9d98d700f3f22667f04cd3db1e94cd7aad
Joaquin (Pato) Decima <jdecima@vasak.net.ar>
2025-06-23 10:03:41 -0300

    Refactor Cargo.toml and lib.rs for consistency and readability

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,8 @@
 use tauri::{
-  plugin::{Builder, TauriPlugin},
-  Manager, Runtime,
+    plugin::{Builder, TauriPlugin},
+    Manager, Runtime,
 };
 
-#[cfg(desktop)]
 mod desktop;
 
 mod commands;
@@ -12,29 +11,29 @@ mod models;
 
 pub use error::{Error, Result};
 
-#[cfg(desktop)]
 use desktop::Vicons;
 
-/// Extensions to [`tauri::App`], [`tauri::AppHandle`] and [`tauri::Window`] to access the vicons APIs.
 pub trait ViconsExt<R: Runtime> {
-  fn vicons(&self) -> &Vicons<R>;
+    fn vicons(&self) -> &Vicons<R>;
 }
 
 impl<R: Runtime, T: Manager<R>> crate::ViconsExt<R> for T {
-  fn vicons(&self) -> &Vicons<R> {
-    self.state::<Vicons<R>>().inner()
-  }
+    fn vicons(&self) -> &Vicons<R> {
+        self.state::<Vicons<R>>().inner()
+    }
 }
 
 /// Initializes the plugin.
 pub fn init<R: Runtime>() -> TauriPlugin<R> {
-  Builder::new("vicons")
-    .invoke_handler(tauri::generate_handler![commands::get_icon, commands::get_symbol])
-    .setup(|app, api| {
-      #[cfg(desktop)]
-      let vicons = desktop::init(app, api)?;
-      app.manage(vicons);
-      Ok(())
-    })
-    .build()
+    Builder::new("vicons")
+        .invoke_handler(tauri::generate_handler![
+            commands::get_icon,
+            commands::get_symbol
+        ])
+        .setup(|app, api| {
+            let vicons = desktop::init(app, api)?;
+            app.manage(vicons);
+            Ok(())
+        })
+        .build()
 }
