a7ec9457ecb8682e5ef7790d19d70fa3d3fd70cd
Joaquin (Pato) Decima <jdecima@vasak.net.ar>
2025-08-19 14:32:41 -0300

    Actualizar la dependencia de base64 a la versión 0.22 y mejorar la legibilidad del código en varios archivos.

--- src/desktop.rs
+++ src/desktop.rs
@@ -2,15 +2,13 @@ use serde::de::DeserializeOwned;
 use tauri::{plugin::PluginApi, AppHandle, Runtime};
 
 pub fn init<R: Runtime, C: DeserializeOwned>(
-  app: &AppHandle<R>,
-  _api: PluginApi<R, C>,
+    app: &AppHandle<R>,
+    _api: PluginApi<R, C>,
 ) -> crate::Result<Vicons<R>> {
-  Ok(Vicons(app.clone()))
+    Ok(Vicons(app.clone()))
 }
 
 /// Access to the vicons APIs.
 pub struct Vicons<R: Runtime>(AppHandle<R>);
 
-impl<R: Runtime> Vicons<R> {
-
-}
+impl<R: Runtime> Vicons<R> {}
