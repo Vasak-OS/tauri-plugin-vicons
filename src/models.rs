e741c83a9fbd7d122c6a35f7045dd1cb88116c04
Joaquin (Pato) Decima <jdecima@vasak.net.ar>
2026-01-20 15:49:03 -0300

    Agregar implementación de Debug a CacheEntry para mejorar la depuración y el registro de datos en caché.

--- src/models.rs
+++ src/models.rs
@@ -1,7 +1,7 @@
 use std::time::SystemTime;
 
 /// Estructura para almacenar datos en caché
-#[derive(Clone)]
+#[derive(Clone, Debug)]
 pub struct CacheEntry {
     pub data: String,
     pub timestamp: SystemTime,
